654e78228ef1fb08e11a7cdb7abd01cb4fadd273
Raof1212 <raofkan5@gmail.com>
2025-08-15 02:39:13 +0100

    Update spedd.lua

--- spedd.lua
+++ spedd.lua
@@ -4,10 +4,10 @@ local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
 
 local LocalPlayer = Players.LocalPlayer
-local HIGH_JUMP_POWER = 40
-local HIGH_WALK_SPEED = 33
-local DEFAULT_JUMP_POWER = 40
-local DEFAULT_WALK_SPEED = 33
+local HIGH_JUMP_POWER = 500
+local HIGH_WALK_SPEED = 100
+local DEFAULT_JUMP_POWER = 500
+local DEFAULT_WALK_SPEED = 1000
 
 local jumpEnabled = false
 local speedEnabled = false
@@ -53,6 +53,7 @@ if LocalPlayer.Character then
         SetupToggles(humanoid)
     end
 end
+
 
 
 
